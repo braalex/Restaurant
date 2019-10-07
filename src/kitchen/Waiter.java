8a9ddd38ce4fcefb460501aee96387af97abdd3c
braalex <haxball3012>
2019-10-07 14:06:59 +0300

    After task5_1

--- src/kitchen/Waiter.java
+++ src/kitchen/Waiter.java
@@ -1,4 +1,13 @@
 package kitchen;
 
-public class Waiter {
+import restaurant.ConsoleHelper;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Waiter implements Observer {
+    @Override
+    public void update(Observable observable, Object order) {
+        ConsoleHelper.writeMessage(order.toString() + " was cooked by " + observable.toString());
+    }
 }
